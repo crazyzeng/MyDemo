5dadab3e5e67dcd1704aa343a0027f470ea007af
286412774@qq.com <286412774@qq.com>
2019-01-22 16:09:41 +0800

    add element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,20 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3{
+  margin: 40px 0 0
+}
+ul{
+  list-style-type :none;
+  padding: 0
+}
+li{
+  display :inline-block;
+  margin: 0 10px;
+}
+a{
+  color :#42b983
+}
+</style>
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
@@ -6,7 +23,8 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Installed CLI1111 Plugins</h3>
+    <el-button>dadadadad</el-button>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
@@ -41,20 +59,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
-</style>
